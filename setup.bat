7497c69951aa57297f4a29273bf7bd7250494cf6
BlackCat <184516521+MeoMunDep@users.noreply.github.com>
2024-12-28 15:06:34 +0700

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -1,5 +1,5 @@
 @echo off
-title #Memhash Bot
+title #Memhash Bot by @MeoMunDep
 color 0A
 
 cd ..
@@ -14,7 +14,7 @@ if exist node_modules (
 :MENU
 cls
 echo =================================================================
-echo    #Memhash BOT SETUP AND RUN SCRIPT
+echo    #Memhash BOT SETUP AND RUN SCRIPT by @MeoMunDep
 echo =================================================================
 echo.
 echo Current directory: %CD%
